e32ab99935194f6da7dd9096d28a3a92fdd40498
Blake Embrey <hello@blakeembrey.com>
2015-12-11 16:17:27 -0800

    Make error returns a constructor function

--- index.d.ts
+++ index.d.ts
@@ -1,28 +1,33 @@
 /**
  * Create a new error constructor instance.
  */
-declare function makeError (name: string): makeError.BaseError;
+declare function makeError(name: string): makeError.BaseErrorConstructor;
 
 /**
  * Set the constructor prototype to `BaseError`.
  */
-declare function makeError <T extends Function> (constructor: T): T & makeError.BaseError;
+declare function makeError<T extends typeof Error>(super_: T): T & makeError.BaseErrorConstructor;
 
 /**
  * Create a specialized error instance.
  */
-declare function makeError <T extends Function> (name: string, constructor: T): T;
+declare function makeError<T extends typeof Error>(name: string | Function, super_: T): T;
 
 declare module makeError {
   /**
-   * Use ES6 inheritance.
+   * Use with ES6 inheritance.
    */
   export class BaseError implements Error {
     message: string;
     name: string;
     stack: string;
 
-    constructor (message: string);
+    constructor(message: string);
+  }
+
+  export interface BaseErrorConstructor {
+    new (message: string): BaseError
+    super_: any
   }
 }
 
